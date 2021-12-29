393b8429e538149ae05750f09c57a45a458c8f94
catman <absobakin@gmail.com>
2021-12-29 16:46:52 +0300

    new version

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mr1jingles/ex6_4/v2
+module github.com/mr1jingles/ex6_4/v3
 
 go 1.17
--- myModule.go
+++ myModule.go
@@ -1,8 +1,14 @@
 package ex6_4
 import (
 	"fmt"
+	"time"
 )
 
 func PrintHello(name string) {
 	fmt.Printf("Hello, %s!\n", name)
+}
+
+func TimeNow() {
+	now := time.Now()
+	fmt.Println(now.Format(time.RFC822))
 }
